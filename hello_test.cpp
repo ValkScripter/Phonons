5ad1641a8cc493af9099e076862dd9eae73148c2
ValkScripter <161381539+ValkScripter@users.noreply.github.com>
2024-12-21 13:35:49 +0000

    Add devcontainer configuration and update CMake for C++20 support
    Solved dependencies for gtest and gsl

--- hello_test.cpp
+++ hello_test.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
-TEST(HelloTest, BasicAssertions) {
+TEST(HelloTest, BasicAssertions)
+{
     // Expect two strings not to be equal.
     EXPECT_STRNE("hello", "world");
     // Expect equality.
